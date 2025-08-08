4ef35adb4dd1fa6fde3429ebc6191d519ac97edb
Manthan Sinojiya <sinojiyamonu@gmail.com>
2025-08-08 09:58:23 +0530

    initial project

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return jsonify({"message": "Hello from Demo DevOps App!"})
 
+
 @app.route("/echo", methods=["POST"])
 def echo():
     data = request.get_json() or {}
-    return jsonify({"you_sent": data}), 201
+    return jsonify({"I'm Manthan Sinojiya.": data}), 201
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
-import json
 from app import app
+
 
 def test_index():
     client = app.test_client()
@@ -7,9 +7,10 @@ def test_index():
     assert r.status_code == 200
     assert r.get_json() == {"message": "Hello from Demo DevOps App!"}
 
+
 def test_echo():
     client = app.test_client()
-    payload = {"name":"sinojiya"}
+    payload = {"name": "sinojiya"}
     r = client.post("/echo", json=payload)
     assert r.status_code == 201
     assert r.get_json()["you_sent"] == payload
